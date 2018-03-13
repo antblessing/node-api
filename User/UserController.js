f3c03f35a986d716ecd521caa2ff14822561d0ab
antblessing <antblessing@gmail.com>
2018-03-12 23:14:23 -0700

    Added PUT, DELETE and GET for specific user

--- User/UserController.js
+++ User/UserController.js
@@ -24,4 +24,26 @@ router.get('/', (req,res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+    res.status(200).send(user);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id, (err, user) => {
+    if (err) return res.status(500).send("There was a problem deleting the user.");
+    res.status(200).send("User " + user.name + " was removed.");
+  });
+});
+
+router.put('/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, user) => {
+    if (err) return res.status(500).send("There was a problem updating the user.");
+    res.status(200).send(user);
+  });
+});
+
 module.exports = router;
